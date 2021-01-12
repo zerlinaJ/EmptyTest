0a356469d808eabad04a717be495d15ec83d5bf2
Zerlina Jacob <zerlinaj@gmail.com>
2021-01-12 10:39:42 +0530

    ground changed to platform

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    platform = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -45,7 +45,7 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
-    ground.display();
+    platform.display();
     pig1.display();
     log1.display();
 
